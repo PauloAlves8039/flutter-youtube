9593d957e6c64c1f562cf43748e47f49c299d559
PauloAlbes8039 <pj38alves@gmail.com>
2021-05-09 20:05:29 -0300

    Adicionando elementos no AppBar

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,6 +8,37 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.grey),
+        backgroundColor: Colors.white,
+        title: Image.asset(
+          "assets/imagens/youtube.png",
+          width: 98,
+          height: 22,
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: () {
+              print("acao: videocam");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("acao: pesquisa");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () {
+              print("acao: conta");
+            },
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
